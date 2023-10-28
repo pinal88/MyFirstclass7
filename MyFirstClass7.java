35a7d2a953709102294813bf584dbd82bcb28e90
Pinal Pema <pinal88pema@gmail.com>
2023-10-28 14:42:46 +0100

    modified

--- MyFirstClass7.java
+++ MyFirstClass7.java
@@ -3,5 +3,7 @@ package myfirstpackage7;
 public class MyFirstClass7 {
     public static void main(String[] args) {
         int a =10;
+        int b =20;
+        System.out.println(a+b); //10+20=30
     }
 }
